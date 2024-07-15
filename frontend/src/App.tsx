98697b908f9882a761165a2c699cf9d1190ed9a1
CoderIrfann <irfankhan9888477@gmail.com>
2024-07-15 23:39:05 +0530

    Updating contactUs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import OurServicesPage from "./pages/Service";
 import AboutUs from "./pages/AboutUs";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ContactUs from './pages/ContactUs'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/service" element={<OurServicesPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/contactus" element={<ContactUs/>} />
       </Routes>
     </div>
   );
